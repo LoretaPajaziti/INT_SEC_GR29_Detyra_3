8d63912d1c5cfc7fb9fb809bebab0404fa43f026
Loreta Pajaziti <61281679+LoretaPajaziti@users.noreply.github.com>
2022-01-09 15:43:34 +0100

    Update ftp_upload.php

--- ftp_upload.php
+++ ftp_upload.php
@@ -1,5 +1,40 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head><body>
+    <title>Upload Files to FTP Server in PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu|Lora">
+    <style>
+        html {
+  height: 100%;
+}
+body {
+ 
+    font-family: sans-serif;
+    background: linear-gradient(#141e30, #243b55);
+    background-image: url("diamond.jpg");
+    background-size: 100%;
 
+}
+p {
+            color: white;
+            font-family: Ubuntu, sans-serif;
+            font-size: 25px;
+	    text-align: center;
+			
+        }
+div {
+
+	border: solid, 2px, yellow; 
+
+}
+
+</style>
+</head>
+</body>
+</html>
+<?php
 // Connect to FTP server
 $ftp_server = "127.0.0.1";
 
@@ -20,10 +55,10 @@ $allowed = array('doc', 'docx','xls','xlsx');
 if(in_array($fileActualExt, $allowed)){
 // Establishing ftp connection
 $ftp_connection = ftp_connect($ftp_server)
-	or die("Could not connect to $ftp_server");
+	or die("<p>Could not connect to $ftp_server </p>");
 
 if( $ftp_connection ) {
-	echo "Successfully connected to the ftp server!";
+	echo "<p style=\"margin-top: 180px\">Successfully connected to the FTP Server!</p>";
 	
 	// Logging in to established connection with
 	// ftp username password
@@ -31,31 +66,32 @@ if( $ftp_connection ) {
 	
 	// Checking whether logged in successfully or not
 	if($login) {
-		echo "<br>Logged in successfully!";
+		echo "<br><p>Logged in successfully!<p/>";
 		
 		if (ftp_put($ftp_connection,$file, $path_file, FTP_BINARY))
 		{
-		echo "<br>Uploaded successful $file.";
+		echo "<br><p>$file uploaded successfully!</p>";
 		}
 		else {
-		echo "<br>Error while uploading $file.";
+		echo "<br><p>Error while uploading $file.</p>";
 		}
 		
 	}
 	else {
-		echo "<br>Login failed!";
+		echo "<br><p>Login failed!</p>";
 	}
 
 	// Closing the connection
 	if(ftp_close($ftp_connection)) {
-		echo "<br>Connection closed Successfully!";
+		echo "<br><p>Connection closed successfully!</p>";
 	}
 }
 
 }
 else {
-  	echo "The type of file you have chosen is not compatible with our system.";
+  	echo "<p style=\"margin-top: 180px\">The type of file you have chosen is not compatible with our system.</p>";
 	echo "<br>";
-	echo "Please select only .docx or .xlsx files!";
+	echo "<p>Please select only .doc, .docx, .xls or .xlsx files!</p>";
    }
+
 ?>
